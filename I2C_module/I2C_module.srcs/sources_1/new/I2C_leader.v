a7b8efeb986ba51e894f6c740999970de217593e
Andriyanto Halim <andriyanto.halim@gmail.com>
2018-10-12 16:15:22 +0800

    add SPI and I2C placeholder projects

---
+++ I2C_module/I2C_module.srcs/sources_1/new/I2C_leader.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12.10.2018 16:14:01
+// Design Name: 
+// Module Name: I2C_leader
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module I2C_leader(
+
+    );
+endmodule
