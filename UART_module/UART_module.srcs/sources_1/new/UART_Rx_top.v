20be8687e1c4cd69094763c6803e529ca251fe92
Andriyanto Halim <andriyanto.halim@gmail.com>
2018-10-12 16:04:57 +0800

    add tops

---
+++ UART_module/UART_module.srcs/sources_1/new/UART_Rx_top.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Halim
+// 
+// Create Date: 12.10.2018 16:01:11
+// Design Name: 
+// Module Name: UART_Rx_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module UART_Rx_top(clk, RsRx, led);
+
+
+endmodule
---
+++ UART_module/UART_module.srcs/sources_1/new/UART_Tx_top.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Halim
+// 
+// Create Date: 12.10.2018 16:01:11
+// Design Name: 
+// Module Name: UART_Tx_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module UART_Tx_top(btnC, clk, RsTx);
+
+
+
+endmodule
