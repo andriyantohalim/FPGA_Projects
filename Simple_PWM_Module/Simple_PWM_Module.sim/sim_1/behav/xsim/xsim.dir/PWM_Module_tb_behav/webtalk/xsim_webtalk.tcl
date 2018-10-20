9a7704236294efa9b10ff1c39a1e470d37917617
Andriyanto Halim <andriyanto.halim@gmail.com>
2018-10-20 17:30:16 +0800

    use PWM_Module for future dev

--- Simple_PWM_Module/Simple_PWM_Module.sim/sim_1/behav/xsim/xsim.dir/PWM_Module_tb_behav/webtalk/xsim_webtalk.tcl
+++ Simple_PWM_Module/Simple_PWM_Module.sim/sim_1/behav/xsim/xsim.dir/PWM_Module_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir D:/FPGA_Projects/Simple_PWM_Module/Simple_PWM_Module.sim/sim_1/behav/xsim/xsim.dir/PWM_Module_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Sun Oct 14 16:25:52 2018" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sat Oct 20 17:00:48 2018" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.2.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2348494" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "0d03e498-7a5a-41f1-9931-ce40f52debaa" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "d1ff4f9eb7b0478ba5d4cb06ebfb1600" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "6" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "8" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i5-8400 CPU @ 2.80GHz" -context "user_environment"
@@ -27,6 +27,6 @@ webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "1" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Time -value "0.00_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "5636_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3237266513 -regid "" -xml D:/FPGA_Projects/Simple_PWM_Module/Simple_PWM_Module.sim/sim_1/behav/xsim/xsim.dir/PWM_Module_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/FPGA_Projects/Simple_PWM_Module/Simple_PWM_Module.sim/sim_1/behav/xsim/xsim.dir/PWM_Module_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/FPGA_Projects/Simple_PWM_Module/Simple_PWM_Module.sim/sim_1/behav/xsim/xsim.dir/PWM_Module_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Memory -value "5632_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 2792192789 -regid "" -xml D:/FPGA_Projects/Simple_PWM_Module/Simple_PWM_Module.sim/sim_1/behav/xsim/xsim.dir/PWM_Module_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/FPGA_Projects/Simple_PWM_Module/Simple_PWM_Module.sim/sim_1/behav/xsim/xsim.dir/PWM_Module_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/FPGA_Projects/Simple_PWM_Module/Simple_PWM_Module.sim/sim_1/behav/xsim/xsim.dir/PWM_Module_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
