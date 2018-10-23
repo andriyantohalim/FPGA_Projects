947d33a16ac7d6835984dd39f67a77e07379f35f
Andriyanto Halim <andriyanto.halim@gmail.com>
2018-10-23 22:02:38 +0800

    count16 module

---
+++ count16_module/count16_module.srcs/sources_1/new/count16_module.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 100ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 22.10.2018 23:11:46
+// Design Name: 
+// Module Name: count16_module
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module count16 (count, count_tri, clk, rst_l, load_l, enable_l, cnt_in, oe_l);
+ output [3:0] count;
+ output [3:0] count_tri;
+ 
+ input clk;
+ input rst_l;
+ input load_l;
+ input enable_l;
+ input [3:0] cnt_in;
+ input oe_l;
+ 
+ reg [3:0] count;
+ 
+ // tri-state buffers
+ assign count_tri = (!oe_l) ? count : 4'bZZZZ;
+ 
+ // synchronous 4 bit counter
+ always @ (posedge clk or negedge rst_l)
+     begin
+         if (!rst_l) begin
+             count <= #1 4'b0000;
+            end
+         else if (!load_l) begin
+             count <= #1 cnt_in;
+            end
+         else if (!enable_l) begin
+             count <= #1 count + 1;
+             end
+     end
+endmodule //of count16
